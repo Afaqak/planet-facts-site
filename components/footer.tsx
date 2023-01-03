1ae148b8d2802c54d8ea9a7f522b1ba8735e8e7d
afaqak <75029026+Afaqak@users.noreply.github.com>
2023-01-03 10:32:31 -0800

    added the footer sections

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
 
-const Footer = () => {
+type Feature = {
+  rotation: string;
+  revolution: string;
+  radius: string;
+};
+
+
+const Footer = ({features}: {features: Feature}) => {
   return (
-    <div>Footer</div>
+    <div className="flex flex-col text-white md:flex-row justify-evenly items-center space-y-4 md:space-y-0">
+      <div className="flex flex-col items-center
+        py-2 px-10 border border-white
+      ">
+        <h1 className="text-lg font-bold">Rotation Time</h1>
+        <p className="text-sm md:text-md">{features.rotation}</p>
+      </div>
+      <div className="flex flex-col items-center
+        py-2 px-10 border border-white
+      ">
+        <h1 className="text-lg font-bold">Revolution Time</h1>
+        <p className="text-sm md:text-md">{features.revolution}</p>
+      </div>
+      <div className="flex flex-col items-center
+        py-2 px-10 border border-white
+      ">
+        <h1 className="text-lg font-bold">Radius covered</h1>
+        <p className="text-sm md:text-md">{features.radius}</p>
+        </div>
+      
+      </div>
+
   )
 }
 
